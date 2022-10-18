79767041ad7dd00ed5f59d00518af280b3377422
denyskuz <denis.kuz96@gmail.com>
2022-10-18 19:54:01 +0000

    Deploying to gh-pages from @ denyskuz/goit-react-hw-08-phonebook@56d6e29b676028d64fda7000b5a3f56e41a6a3f0 🚀

---
+++ static/js/436.9807cbec.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[436],{3436:function(e,a,n){n.r(a),n.d(a,{default:function(){return l}});var r="Home_container__80jOb",t="Home_title__jSxc5",c=n(184);function l(){return(0,c.jsx)("div",{className:r,children:(0,c.jsxs)("h1",{className:t,children:["Task manager welcome page"," ",(0,c.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81"})]})})}}}]);
+//# sourceMappingURL=436.9807cbec.chunk.js.map
